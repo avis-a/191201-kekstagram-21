4857b4781cd8638e5caf50d422a0045b559b8f78
Alina Glebova <alina.lina2407@gmail.com>
2020-10-07 15:17:59 +0300

    Сделала второе дз

--- js/main.js
+++ js/main.js
@@ -54,7 +54,7 @@ var generateRandomPhotos = function () {
 
   for (var i = 0; i < COUNT_PHOTOS; i++) {
     photos[i] = {
-      url: `photos/${i}.jpg`,
+      url: `photos/${i + 1}.jpg`,
       description: 'test',
       likes: getRandomInt(LIKES.min, LIKES.max),
       comments: createComments()
@@ -89,3 +89,169 @@ var appendPhotos = function (photos) {
 
 let photos = generateRandomPhotos();
 appendPhotos(photos);
+
+// Загрузка изображения и показ формы редактирования
+var uploadFile = document.querySelector('#upload-file');
+var uploadCancel = document.querySelector('#upload-cancel');
+var uploadOverlay = document.querySelector('.img-upload__overlay');
+
+var hashtagInput = document.querySelector('.text__hashtags');
+
+uploadFile.onchange = function () {
+  uploadOverlay.classList.remove('hidden');
+  document.querySelector('body').classList.add('modal-open');
+  document.addEventListener('keydown', escPress);
+};
+
+let closeModal = function () {
+  uploadOverlay.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+};
+
+uploadCancel.onclick = function () {
+  closeModal();
+};
+
+var escPress = function (evt) {
+  if (evt.key === 'Escape' && document.activeElement !== hashtagInput) {
+    evt.preventDefault();
+    closeModal();
+  }
+};
+
+// Применение эффекта для изображения
+var effects = document.querySelectorAll('.effects__radio');
+var imgPreview = document.querySelector('.img-upload__preview');
+var effectLevel = document.querySelector('.effect-level__pin');
+var effectLevelValue = document.querySelector('.effect-level__value');
+
+const EFFECT_VALUES = {
+  'chrome': {
+    min: 0,
+    max: 1,
+    getFilter(value) {
+      return `grayscale(${value})`;
+    }
+  },
+  'sepia': {
+    min: 0,
+    max: 1,
+    getFilter(value) {
+      return `sepia(${value})`;
+    }
+  },
+  'marvin': {
+    min: 0,
+    max: 100,
+    getFilter(value) {
+      return `invert(${value}%)`;
+    }
+  },
+  'phobos': {
+    min: 0,
+    max: 3,
+    getFilter(value) {
+      return `blur(${value}px)`;
+    }
+  },
+  'heat': {
+    min: 1,
+    max: 3,
+    getFilter(value) {
+      return `brightness(${value})`;
+    }
+  },
+};
+
+let effectsIntensive = function (effect) {
+  if (effect.value !== 'none') {
+    let min = EFFECT_VALUES[effect.value].min;
+    let max = EFFECT_VALUES[effect.value].max;
+    let result = min + ((max - min) / 100 * effectLevelValue.value);
+    imgPreview.style = 'filter: ' + EFFECT_VALUES[effect.value].getFilter(result);
+  } else {
+    imgPreview.style = null;
+  }
+};
+
+effects.forEach(function (effect) {
+  effect.onchange = function () {
+    imgPreview.classList = null;
+    imgPreview.classList.add('img-upload__preview');
+    imgPreview.classList.add('effects__preview--' + effect.value);
+
+    effectLevelValue.value = 100;
+    effectsIntensive(effect);
+  };
+});
+
+effectLevel.mouseup = function () {
+  let effect = document.querySelector('input[name=effect]:checked');
+  effectsIntensive(effect);
+};
+
+// Редактирование размера изображения
+var controlSmaller = document.querySelector('.scale__control--smaller');
+var controlBigger = document.querySelector('.scale__control--bigger');
+var controlValue = document.querySelector('.scale__control--value');
+controlValue.value = '100%';
+
+const ZOOM_STEP = 25;
+
+const ZOOM_LIMITS = {
+  min: 25,
+  max: 100
+};
+
+controlSmaller.onclick = function () {
+  let percentNumber = Number(controlValue.value.replace('%', ''));
+  percentNumber -= ZOOM_STEP;
+
+  if (percentNumber >= ZOOM_LIMITS.min) {
+    controlValue.value = percentNumber + '%';
+    imgPreview.style = `transform: scale(${percentNumber / 100})`;
+  }
+};
+
+controlBigger.onclick = function () {
+  let percentNumber = Number(controlValue.value.replace('%', ''));
+  percentNumber += ZOOM_STEP;
+
+  if (percentNumber <= ZOOM_LIMITS.max) {
+    controlValue.value = percentNumber + '%';
+    imgPreview.style = `transform: scale(${percentNumber / 100})`;
+  }
+};
+
+// Валидация хеш-тегов
+
+const re = /^#[A-я\d]*$/;
+
+hashtagInput.addEventListener(`input`, function () {
+  let hashtags = hashtagInput.value.split(' ').filter((x) => x !== '');
+
+  if (hashtags.length === 0) {
+    return;
+  } else if (hashtags.length > 5) {
+    hashtagInput.setCustomValidity('Нельзя указать больше пяти хэш-тегов.');
+    return;
+  }
+
+  for (var hashtag of hashtags) {
+    if (hashtag === '#') {
+      hashtagInput.setCustomValidity('Хэштег не может состоять только из одной решётки.');
+    } else if (!hashtag.startsWith('#')) {
+      hashtagInput.setCustomValidity('Хэштег должен начинаться с символа # (решётка).');
+    } else if (hashtag.length >= 20) {
+      hashtagInput.setCustomValidity('Максимальная длина одного хэш-тега 20 символов, включая решётку.');
+    } else if (hashtags.filter((x) => x.toLowerCase() === hashtag.toLowerCase()).length > 1) {
+      hashtagInput.setCustomValidity('Такой хэштег уже есть.');
+    } else if (!re.test(hashtag)) {
+      hashtagInput.setCustomValidity('Значение не может содержать спец. символы');
+    } else {
+      hashtagInput.setCustomValidity(``);
+    }
+  }
+  hashtagInput.reportValidity();
+});
+
