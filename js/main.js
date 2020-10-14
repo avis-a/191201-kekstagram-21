7f9faf91d9a50ecd80ed6dcc3bb6c72342538370
Alina Glebova <alina.lina2407@gmail.com>
2020-10-14 16:27:59 +0300

    Сделала взаимодействие с удалённым сервером

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 'use strict';
 
-const photos = window.data.generateRandomPhotos();
-window.gallery.appendPhotos(photos);
+// const photos = window.data.generateRandomPhotos();
+// window.gallery.appendPhotos(photos);
+
+(function () {
+  var onError = function (message) {
+    console.error(message);
+  };
+
+  var onSuccess = function (data) {
+    window.gallery.appendPhotos(data);
+  };
+
+  window.load('https://21.javascript.pages.academy/kekstagram/data', onSuccess, onError);
+})();
